c4f5d2bebd434d21c1734c23f6f4cd2ffb4ae218
sude <sudefidan@icloud.com>
2024-10-12 20:41:01 +0300

    c# giriş

---
+++ HAFTA-1/1.GirisDersi/GirisDersi/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GirisDersi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write Komutu tanıtıldı
+
+            Console.WriteLine("Merhaba Dünya");
+            Console.WriteLine("Enes Aktaş");
+            Console.WriteLine(2);
+
+            Console.WriteLine();
+            Console.WriteLine("-------------------");
+            Console.WriteLine();
+
+            Console.Write("SmartPro'ya");
+            Console.WriteLine(" Hosgeldiniz");
+
+            //Değişkenler
+
+            string metin = "SmartPro'ya Hosgeldiniz!!!";
+            Console.WriteLine(metin);
+
+            string adSoyad = "Enes Aktaş";
+            Console.WriteLine(adSoyad);
+
+            string ad = "Bayram";
+            string soyad = "Coskun";
+            Console.WriteLine(ad + " " + soyad);
+
+
+            Console.WriteLine();
+            Console.WriteLine("-------------------");
+            Console.WriteLine();
+
+            string isim = "Bayram";
+            string soyIsım = "Coskun";
+
+            Console.WriteLine("Benim adım " + isim + " soyadım ise " + soyIsım);
+
+
+            Console.WriteLine();
+            Console.WriteLine("-------------------");
+            Console.WriteLine();
+
+            int sayi = 0;
+            Console.WriteLine(sayi);
+
+            int yas = 25;
+            Console.WriteLine(yas);
+
+            int sayi1 = 10;
+            int sayi2 = 7;
+            int sayi3 = 19;
+
+            Console.WriteLine(sayi1 + " " + sayi2);
+
+            Console.WriteLine("Benim yaşım : " + yas);
+
+            //Birinci sayi = 10 , İkinci sayi = 7 , Üçüncü sayi = 19
+
+            Console.WriteLine("Birinci sayi : " + sayi1 + " İkinci sayi : " + sayi2 + " Üçüncü sayi : " + sayi3);
+
+            /*Birinci sayi = 10
+             * İkinci Sayi = 7
+             * Üçüncü sayi = 19 */
+
+            Console.WriteLine("\nBirinci sayi : " + sayi1 + "\nİkinci Sayi : " + sayi2 + "\nÜçüncü Sayi : " + sayi3);
+
+            int rakam1 = 5;
+            int rakam2 = 9;
+            int rakam3 = 7;
+            int sonuc = rakam1 + rakam2 + rakam3;
+
+            Console.WriteLine("Bu üç sayının toplamı : " + sonuc);
+
+
+            Console.WriteLine();
+            Console.WriteLine("-------------------");
+            Console.WriteLine();
+
+            /*Ad Soyad = Enes Aktas ,
+             * Yaş = 25 ,
+             * Memleket = Elazığ ,
+             * CepNo = 132154*/
+
+            string nameSurname = "Enes Aktaş";
+            int age = 25;
+            string homeTown = "Elazığ";
+            string phoneNumber = "123456789";
+
+            Console.WriteLine("Ad Soyad = " + nameSurname + "\nYaş = " + age + "\nMemleket = " + homeTown + "\nCepNo = " + phoneNumber);
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
---
+++ HAFTA-1/Ders2/Ders2/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //// int sayi;
+            //// Console.WriteLine("Lütfem bir sayi giriniz : ");
+            ////// sayi = Convert.ToInt32(Console.ReadLine());
+            //// sayi = int.Parse(Console.ReadLine());
+            ///
+
+
+
+            //// Console.WriteLine("Girdiğiniz sayi : " + sayi);
+
+            // Console.WriteLine();
+            // Console.WriteLine("---------------------");
+            // Console.WriteLine();
+
+            // /* == eşit
+            //  * <= küçük eşit
+            //  * >= büyük eşit
+            //  * != eşit degil
+            //  * && ve
+            //  * || veya 
+            //  */
+
+
+            // string ad;
+            // Console.WriteLine("Bir isim giriniz : ");
+
+            // ad = Console.ReadLine();
+
+            // if (ad == "SmartPro")
+            // {
+            //     Console.WriteLine("Girdiğiniz ifade " + ad);
+            // }
+            // else if (ad == "Kadir")
+            // {
+            //     Console.WriteLine("Girdiğiniz ifade " + ad);
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Hatalı Giriş");
+            // }
+
+
+            // Console.WriteLine();
+            // Console.WriteLine("---------------------");
+            // Console.WriteLine();
+
+
+            // int sinav1, sinav2, ortalama;
+
+            // Console.Write("1.Sınav : ");
+            // sinav1 = int.Parse(Console.ReadLine());
+            // Console.Write("2.Sınav : ");
+            // sinav2 = int.Parse(Console.ReadLine());
+
+            // ortalama = (sinav1 + sinav2) / 2;
+
+            // if (ortalama >= 50 && sinav1 >= 0 && sinav2 >= 0 && sinav1 <= 100 && sinav2 <= 100)
+            // {
+            //     Console.WriteLine("Tebrikler. Dersten geçtiniz. Ortalamanız : " + ortalama);
+            // }
+            // else if (ortalama < 50 && sinav1 >= 0 && sinav2 >= 0 && sinav1 <= 100 && sinav2 <= 100)
+            // {
+            //     Console.WriteLine("Üzgünüm. Dersten kaldınız. Ortalamanız : " + ortalama);
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Sınav notları negatif ve 100den büyük değer alamaz!!!");
+            // }
+
+
+            // Console.WriteLine();
+            // Console.WriteLine("---------------------");
+            // Console.WriteLine();
+
+
+            // // Vize %40 , Final %60, ortalama 50den aşağı kalır, 50 ve üstü geçer, vize final notları 0-100 arasında değer almak zorundadır.
+
+            // double vize, final, ort;
+
+            // Console.Write("Vize Notu : ");
+            // vize = double.Parse(Console.ReadLine());
+            // Console.Write("Final Notu : ");
+            // final = double.Parse(Console.ReadLine());
+
+            // ort = (vize * 40 / 100) + (final * 60 / 100);
+
+            // if (ort < 50 && vize <=100 && final<=100 && vize>0 && final>0)
+            // {
+            //     Console.WriteLine("Dersten kaldınız : " + ort);
+            // }
+            // else if (ort >= 50 && vize <= 100 && final <= 100 && vize > 0 && final > 0)
+            // {
+            //     Console.WriteLine("Dersten geçtiniz : " + ort);
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Vize ve Final notları 0-100 arasında olmak zorundadır.");
+            // }
+
+            //Console.WriteLine();
+            //Console.WriteLine("---------------------");
+            //Console.WriteLine();
+
+
+            //int sayi;
+            //Console.Write("Bir sayinin tek mi çift mi oldugunu bulmak için lütfen bir sayi giriniz : ");
+            //sayi = int.Parse(Console.ReadLine());
+
+            //if (sayi % 2 == 0)
+            //{
+            //    Console.WriteLine("Çift");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Tek");
+            //}
+
+            Console.WriteLine();
+            Console.WriteLine("---------------------");
+            Console.WriteLine();
+        }
+    }
+}
